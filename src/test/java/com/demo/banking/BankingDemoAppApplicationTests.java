fcbb7e274deefb53bfc91bf9484b04ece30e5c90
Robert Popa <robert.popa87@gmail.com>
2025-07-22 00:31:42 +0300

    Disable RabitMQ for App tests

--- src/test/java/com/demo/banking/BankingDemoAppApplicationTests.java
+++ src/test/java/com/demo/banking/BankingDemoAppApplicationTests.java
@@ -2,8 +2,12 @@ package com.demo.banking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+})
 class BankingDemoAppApplicationTests {
 
 	@Test
